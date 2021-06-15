5de3482c3f435e9bc45b5425f01a2151c094401c
hkidh <hki.dh4@gmail.com>
2021-06-15 09:34:41 +0800

    second

--- password_trying.py
+++ password_trying.py
@@ -1,15 +1,15 @@
 password = 'a123456'
 x = 3
-while x > 0:
+while x <= 3:
 	x = x - 1
 	pwd = input('請輸入密碼:')
 	if pwd == password:
 		print('登入成功!')
 		break
 	else:
-		print('密碼錯誤')
-		if x > 0:
-			print('還有', x,'次機會')
+		print('密碼錯誤!')
+		if x <= 0:
+			print('沒有機會了')
+			break
 		else:
-			print('沒機會了')	
-	
+			print('還有', x,'次機會')
